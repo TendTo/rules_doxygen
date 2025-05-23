8bf77ffacc7a9047536aa16f6dd08b70a802d488
Ernesto Casablanca <casablancaernesto@gmail.com>
2025-05-23 11:40:47 +0100

    docs: add dependencies example

---
+++ examples/dependencies/add.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file add.cpp
+ * @author Ernesto Casablanca (casablancaernesto@gmail.com)
+ * @copyright 2024
+ */
+
+#include "add.h"
+
+namespace lib {
+
+int add(int a, int b) { return a + b; }
+
+}
---
+++ examples/dependencies/sub.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file sub.cpp
+ * @author Ernesto Casablanca (casablancaernesto@gmail.com)
+ * @copyright 2024
+ */
+
+#include "sub.h"
+
+namespace lib {
+
+int sub(int a, int b) { return a - b; }
+
+}  // namespace lib
